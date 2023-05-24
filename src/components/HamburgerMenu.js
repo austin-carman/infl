50e3066621fd1a9cbbb41779cfc31a7d878544dd
austin-carman <abcarman12@gmail.com>
2023-05-23 15:22:55 -1000

    flexbox styling for name and menu

---
+++ src/components/HamburgerMenu.js
@@ -0,0 +1,11 @@
+const HamburgerMenu = () => {
+  return (
+    <div className="hamburger-menu">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
+
+export default HamburgerMenu;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import HamburgerMenu from "../HamburgerMenu";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h3>Name</h3>
+      <HamburgerMenu />
+    </nav>
+  );
+};
+
+export default Navbar;
