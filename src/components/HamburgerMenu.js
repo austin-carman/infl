a54983d70cbd7dddce7feeed5ebac49281e345f0
austin-carman <abcarman12@gmail.com>
2023-05-24 10:49:39 -1000

    navbar styles

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -1,10 +1,35 @@
+import { useState } from "react";
+
 const HamburgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="hamburger-menu">
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
+    <>
+      {isOpen ? (
+        <div className="expanded-menu">
+          <ul className="menu-items">
+            <li>Home</li>
+            <li>About</li>
+            <li>Destinations</li>
+            <li>Shop</li>
+            <li>Contact</li>
+          </ul>
+          <div className="close-menu" onClick={handleClick}>
+            X
+          </div>
+        </div>
+      ) : (
+        <div className={`hamburger-icon`} onClick={handleClick}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,31 @@
 import HamburgerMenu from "../HamburgerMenu";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const handleShowMenu = () => {
+    setMenuIsOpen(true);
+  };
+
+  const handleCloseMenu = () => {
+    setMenuIsOpen(false);
+  };
+
   return (
     <nav>
       <h3>Name</h3>
-      <HamburgerMenu />
+      <HamburgerMenu
+        handleShowMenu={handleShowMenu}
+        handleCloseMenu={handleCloseMenu}
+      />
+      {menuIsOpen && (
+        <ul>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+        </ul>
+      )}
     </nav>
   );
 };
